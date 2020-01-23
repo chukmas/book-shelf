7942ebbc05c878f9c33ab90ff45bbc44f2f214dc
coparaeke@yahoo.com <coparaeke@yahoo.com>
2020-01-23 15:27:39 +0100

    initial author route

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const express = require("express")
 const app =  express()
 const index = require("./routes/index")
+const authors = require("./routes/authors")
 const mongoose = require("mongoose")
 require("dotenv").config()
 const expressLayouts = require("express-ejs-layouts")
+app.use(express.urlencoded({limit:"10mb",extended:false}))
 
 app.use(expressLayouts)
 app.set("view engine", "ejs")
 
 
 app.use("/", index)
+app.use("/authors", authors)
 mongoose.connect(process.env.DATABASE_URL,{ useNewUrlParser: true,useUnifiedTopology: true },
 (err)=>{
     if(!err){console.log("connected to mongoose")}
